c83d9afdf701cbf3c54a9a3dcc54195f9824344e
safwan <safwanazman017@gmail.com>
2022-02-21 23:17:31 +0800

    feat: add success response & error response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,37 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Success response method.
+     * @param $message
+     * @param $data  
+     * @param $code 
+     */
+    public function successResponse($message,$data,$code = 200)
+    {
+        $response = [
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ];
+
+        return response()->json($response, $code);
+    }
+
+    /**
+     * Error response method.
+     * @param $message
+     * @param $data  
+     * @param $code 
+     */
+    public function errorResponse($message, $code = 400)
+    {
+        $response = [
+            'success' => false,
+            'message' => $message,
+        ];
+
+        return response()->json($response, $code);
+    }
 }
